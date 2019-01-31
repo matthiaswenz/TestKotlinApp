fb39def889e8c4b4dd9ecd465264185d2c753214
Matthias Wenz <matwen@microsoft.com>
2019-01-30 22:09:14 -0800

    Initial commit

---
+++ app/src/main/java/com/microsoft/testkotlinapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.microsoft.testkotlinapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
